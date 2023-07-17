5809601bf463154c4e9def7af8304e190e4d5501
ΓΕΩΡΓΙΟΣ ΤΣΙΑΛΙΟΣ <up1072868@upnet.gr>
2023-07-17 10:34:11 +0300

    All constraints added

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import pulp
 import random
 
-# print(2 and 1)
-
 random.seed(1)
 
 # SETS
@@ -46,16 +44,16 @@ for i in T:
 E = pulp.LpVariable.dicts("E", [(i) for i in T], cat= pulp.LpInteger)
 
 for i in T:
-    if i == 'ΒΙΓΙΑΡΕΜΑΛ' or i=='ΜΠΥΡΑΚΛΗΣ':
+    # if i == 'ΒΙΓΙΑΡΕΜΑΛ' or i == 'ΜΠΥΡΑΚΛΗΣ':
         E[(i)] = 1
-    else:
-        E[(i)] = 0
+    # else:
+    #     E[(i)] = 0
 
 # PROBLEM SET UP
 
 timetable = pulp.LpProblem("Weekly timetable", pulp.LpMaximize)
 
-# Variables
+# DECISION VARIABLES
 
 x = pulp.LpVariable.dicts("x", [(i, j, d) for i in T for j in T if i!=j for d in D], cat= pulp.LpBinary)
 
@@ -74,7 +72,7 @@ x = pulp.LpVariable.dicts("x", [(i, j, d) for i in T for j in T if i!=j for d in
 # print(x)
 
 
-# Objective function
+# OBJECTIVE FUNCTION
 # timetable += \
 #             (40/8) * pulp.lpSum(x[(i,j,d)] for i in T for j in T if i!=j for d in D) \
 #         + (40/48)  * pulp.lpSum((x[(i,j,d)] for j in T if j!=i) * (P[(i, p, d)] % (P[(i, p, d)]-1) for p in range(1,7)) for i in T for d in D) \
@@ -87,26 +85,41 @@ timetable += \
         # +   (40 / 48) * pulp.lpSum(P[(i, p, d)] % (P[(i, p, d)]-1) for i in T for d in D for j in T if j!=i and x[(i,j,d)] == 1 for p in range(1,7)) \
         # +  (20 / 480) * pulp.lpSum(P[(i, p, d)] for i in T for d in D for j in T if j!=i and x[(i,j,d)] == 1 for p in range(1,7)) \
 
-# Constraints
+# CONSTRAINTS
+
+# 1) A team can play:
+#    a) up to once per week if they are not behind in games played
+#    b) up to twice per week if they are behind in games played
 
 for i in T:
     timetable += pulp.lpSum(x[(i,j,d)] for j in T if j!=i for d in D) <= 1 + (E[(i)] and 1)
 
-# for i in T:
-#     for j in T:
-#         if i != j:
-#                 timetable += pulp.lpSum(x[(i,j,d)] for d in D) <= 1, f"c2.{i}.{j}"
+# 2) A team can only play once vs another team per week
+
+for i in T:
+    for j in T:
+        if i != j:
+                timetable += pulp.lpSum(x[(i,j,d)] for d in D) <= 1
+
+
+# 3) A team (that is behind in games played) can not play two consecutive days
 
 for i in T:
      for k in range(len(D)-1):
         timetable += (pulp.lpSum(x[(i,j,D[k])] for j in T if j!=i) + pulp.lpSum(x[(i,j,D[k+1])] for j in T if j!=i) <= 1)
 
+# 4) Max 1 game per day can be played
+
 for d in D:
     timetable += pulp.lpSum(x[(i,j,d)] for i in T for j in T if j!=i) <= 2
+
+# 5) A team can play when at least 4 of their players are available
 
 for i in T:
     for d in D:
         timetable += pulp.lpSum(x[(i,j,d)] for j in T if j!=i) * pulp.lpSum(P[(i, p, d)] % (P[(i, p, d)]-1) for p in range(1,7)) >= 4 * pulp.lpSum(x[(i,j,d)] for j in T if j!=i)
+
+# 6) Home and away games are the same
 
 for i in T:
     for j in T:
