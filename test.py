0f2e8c5d267765696d2042140ac94a1d813ea78d
ΓΕΩΡΓΙΟΣ ΤΣΙΑΛΙΟΣ <up1072868@upnet.gr>
2023-07-18 18:04:12 +0300

    early version of for working

--- test.py
+++ test.py
@@ -25,6 +25,8 @@ D = [
     'Friday'
 ]
 
+matchesPlayed = []
+
 for w in range(1, 9):
 
     random.seed(1)
@@ -49,11 +51,11 @@ for w in range(1, 9):
     E = pulp.LpVariable.dicts("E", [(i) for i in T], cat= pulp.LpInteger)
 
     for i in T:
-        if i == 'ΒΙΓΙΑΡΕΜΑΛ' or i == 'ΜΠΥΡΑΚΛΗΣ':
-                E[(i)] = 1
-        elif i == 'ΜΠΑΡΤΣΕΛΙΩΜΑ' or i == 'ΡΕAΛ MANTPI':
-                E[(i)] = 2
-        else:
+        # if i == 'ΒΙΓΙΑΡΕΜΑΛ' or i == 'ΜΠΥΡΑΚΛΗΣ':
+        #         E[(i)] = 1
+        # elif i == 'ΜΠΑΡΤΣΕΛΙΩΜΑ' or i == 'ΡΕAΛ MANTPI':
+        #         E[(i)] = 2
+        # else:
                 E[(i)] = 0
 
     numberTeamsDouble = sum(E[(i)] and 1 for i in T)
@@ -65,7 +67,7 @@ for w in range(1, 9):
 
     # DECISION VARIABLES
 
-    x = pulp.LpVariable.dicts("x", [(i, j, d) for i in T for j in T if i!=j for d in D], cat= pulp.LpBinary)
+    x = pulp.LpVariable.dicts("x", [(i, j, d) for i in T for j in T if i != j and [i,j] not in matchesPlayed for d in D], cat= pulp.LpBinary)
 
     # for i in T:
     #     for j in T:
@@ -86,15 +88,15 @@ for w in range(1, 9):
 
     if (sum(E.values())>=2):
         timetable += \
-                    (40 / 10) * pulp.lpSum(x[(i, j, d)] for i in T for j in T if i != j for d in D) \
-                +   (40 / 60) * pulp.lpSum(x[(i, j, d)] for i in T for j in T if i != j for d in D  for p in range(1,7) if P[(i, p, d)] and 1) \
-                +   (10 / 600) * pulp.lpSum(x[(i, j, d)] * P[(i, p, d)] for i in T for j in T if i != j for d in D  for p in range(1,7) if P[(i, p, d)] ) \
-                +   (10 / min(10, 2 * numberTeamsDouble)) * pulp.lpSum(x[(i, j, d)] * (E[(i)] and 1) for i in T for j in T if i != j for d in D)
+                    (40 / 10) * pulp.lpSum(x[(i, j, d)] for i in T for j in T if i != j and [i,j] not in matchesPlayed for d in D) \
+                +   (40 / 60) * pulp.lpSum(x[(i, j, d)] for i in T for j in T if i != j and [i,j] not in matchesPlayed for d in D  for p in range(1,7) if P[(i, p, d)] and 1) \
+                +   (10 / 600) * pulp.lpSum(x[(i, j, d)] * P[(i, p, d)] for i in T for j in T if i != j and [i,j] not in matchesPlayed for d in D  for p in range(1,7) if P[(i, p, d)] ) \
+                +   (10 / min(10, 2 * numberTeamsDouble)) * pulp.lpSum(x[(i, j, d)] * (E[(i)] and 1) for i in T for j in T if i != j and [i,j] not in matchesPlayed for d in D)
     else:
         timetable += \
-                        (40 / 8) * pulp.lpSum(x[(i, j, d)] for i in T for j in T if i != j for d in D) \
-                    +   (40 / 48) * pulp.lpSum(x[(i, j, d)] for i in T for j in T if i != j for d in D  for p in range(1,7) if P[(i, p, d)] and 1) \
-                    +   (20 / 480) * pulp.lpSum(x[(i, j, d)] * P[(i, p, d)] for i in T for j in T if i != j for d in D  for p in range(1,7) if P[(i, p, d)] ) \
+                        (40 / 8) * pulp.lpSum(x[(i, j, d)] for i in T for j in T if i != j and [i,j] not in matchesPlayed for d in D) \
+                    +   (40 / 48) * pulp.lpSum(x[(i, j, d)] for i in T for j in T if i != j and [i,j] not in matchesPlayed for d in D  for p in range(1,7) if P[(i, p, d)] and 1) \
+                    +   (20 / 480) * pulp.lpSum(x[(i, j, d)] * P[(i, p, d)] for i in T for j in T if i != j and [i,j] not in matchesPlayed for d in D  for p in range(1,7) if P[(i, p, d)] ) \
 
     # CONSTRAINTS
 
@@ -103,13 +105,13 @@ for w in range(1, 9):
     #    b) up to twice per week if they are behind in games played
 
     for i in T:
-        timetable += pulp.lpSum(x[(i,j,d)] for j in T if j!=i for d in D) <= 1 + (E[(i)] and 1)
+        timetable += pulp.lpSum(x[(i,j,d)] for j in T if i != j and [i,j] not in matchesPlayed for d in D) <= 1 + (E[(i)] and 1)
 
     # 2) A team can only play once vs another team per week
 
     for i in T:
         for j in T:
-            if i != j:
+            if i != j and [i,j] not in matchesPlayed:
                     timetable += pulp.lpSum(x[(i,j,d)] for d in D) <= 1
 
 
@@ -117,24 +119,24 @@ for w in range(1, 9):
 
     for i in T:
         for k in range(len(D)-1):
-            timetable += (pulp.lpSum(x[(i,j,D[k])] for j in T if j!=i) + pulp.lpSum(x[(i,j,D[k+1])] for j in T if j!=i) <= 1)
+            timetable += (pulp.lpSum(x[(i,j,D[k])] for j in T if i != j and [i,j] not in matchesPlayed) + pulp.lpSum(x[(i,j,D[k+1])] for j in T if i != j and [i,j] not in matchesPlayed) <= 1)
 
     # 4) Max 1 game per day can be played
 
     for d in D:
-        timetable += pulp.lpSum(x[(i,j,d)] for i in T for j in T if j!=i) <= 2
+        timetable += pulp.lpSum(x[(i,j,d)] for i in T for j in T if i != j and [i,j] not in matchesPlayed) <= 2
 
     # 5) A team can play when at least 4 of their players are available
 
     for i in T:
         for d in D:
-            timetable += pulp.lpSum(x[(i,j,d)] for j in T if j!=i) * pulp.lpSum(P[(i, p, d)] % (P[(i, p, d)]-1) for p in range(1,7)) >= 4 * pulp.lpSum(x[(i,j,d)] for j in T if j!=i)
+            timetable += pulp.lpSum(x[(i,j,d)] for j in T if i != j and [i,j] not in matchesPlayed) * pulp.lpSum(P[(i, p, d)] % (P[(i, p, d)]-1) for p in range(1,7)) >= 4 * pulp.lpSum(x[(i,j,d)] for j in T if i != j and [i,j] not in matchesPlayed)
 
     # 6) Home and away games are the same
 
     for i in T:
         for j in T:
-            if i!=j:
+            if i !=j and [i,j] not in matchesPlayed:
                 for d in D:
                     timetable += x[(i,j,d)] == x[(j,i,d)]
 
@@ -174,14 +176,14 @@ for w in range(1, 9):
     for match in x:
         if x[match].varValue == 1:
             weeklyMatches.append(match)
+    for match in weeklyMatches:
+        matchesPlayed.append([match[0], match[1]])
 
     weeklyMatches.sort(key=lambda x: D.index(x[2]))
     singleMatches = []
     for index in range(len(weeklyMatches)-1):
         if index % 2 == 0:
             singleMatches.append(weeklyMatches[index])
-    # for match in singleMatches:
-    #     print(f"{match[0]} - {match[1]} - {match[2]}")
     for day in D:
         counter = 0
         for i in range(len(singleMatches)):
@@ -248,11 +250,24 @@ for w in range(1, 9):
     # GPDI
     if numberTeamsDouble > 0:
         print("\t       GPDI\n")
-        print(f"Teams behind in games played, play {(sum(x[(i, j, d)].varValue * (E[(i)] and 1) for i in T for j in T if i != j for d in D)):1.0f} times (max {min(10,2 * numberTeamsDouble)})\n")
+        print(f"Teams behind in games played, play {(sum(x[(i, j, d)].varValue * (E[(i)] and 1) for i in T for j in T if i != j and [i,j] not in matchesPlayed for d in D)):1.0f} times (max {min(10,2 * numberTeamsDouble)})\n")
+
+    # print(matchesPlayed)
+    # if ['ΡΕAΛ MANTPI', 'ΜΠΥΡΑΚΛΗΣ'] in matchesPlayed:
+    #     print('true')
 
     if w < 8:
-        cont = input("Continue? (y/n): ")
-        if cont == "y" or cont == "Y" or cont == "yes" or cont == "Yes" or cont == "YES":
-            continue
-        else:
+        if len(matchesPlayed) == 56:
+            print("Tournament finished. All matches played!\n")
             break
+        else:
+            cont = input("Continue? (y/n): ")
+            if cont == "y" or cont == "Y" or cont == "yes" or cont == "Yes" or cont == "YES":
+                continue
+            else:
+                break
+    else:
+        if len(matchesPlayed) == 56:
+            print("Tournament finished. All matches played!\n")
+        else:
+            print(f"Tournament finished. {56-len(matchesPlayed)} matches not played!\n")
